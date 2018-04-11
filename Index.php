b7bee694d495a35ba19a43736edee7a094dc910c
Berenger Desgardin <berenger.desgardin@gmail.com>
2018-04-11 11:54:08 +0200

    Ajout page accueil

--- Index.php
+++ Index.php
@@ -1,8 +1,22 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: beren
- * Date: 10/04/2018
- * Time: 11:33
- */
-echo '<h1> Hello World </h1>'
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bonjour</title>
+</head>
+<body>
+
+    <h1>La page du bonjour</h1>
+    <ul>
+        <li>Bonjour</li>
+        <li>Hello</li>
+        <li>Hola</li>
+        <li>Konnichiwa</li>
+        <li>Hallo</li>
+    </ul>
+
+</body>
+</html>
